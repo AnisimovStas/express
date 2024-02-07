af7cd90c144ecd13983ae5f372b65efd501558c3
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2024-02-07 11:41:24 +0400

    added some middleware

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,42 @@
-import express, {Request} from "express";
+import express, {NextFunction, Request, Response} from "express";
 import {getCurrenciesRoutes} from "./routes/currencies";
- 
+
 
 const app = express()
 
 export const jsonBodyMiddleware = express.json()
+const testMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    //@ts-ignore
+    req.test = 'test';
 
+    next();
+
+}
+
+const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.token === 'secret') {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+let responseCounter = 0;
+
+const responseCounterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    responseCounter++;
+    next();
+}
 
 app.use(jsonBodyMiddleware);
-
+app.use(testMiddleware);
+app.use(authGuardMiddleware);
+app.use(responseCounterMiddleware);
 
 app.use('/currencies', getCurrenciesRoutes());
 app.get('/', (req: Request, res) => {
-    res.sendStatus(200)
+    const msg = 'total response count: ' + responseCounter;
+    res.status(200).send(msg);
 })
 
 
